19c3521d8d00bb17df1c5fbf3812748fcd0a2da4
Gabriel MP <contato.gabrielmartinspadoin@gmail.com>
2023-07-17 19:05:40 -0300

    [TSHB] the_basics - feat: Emitting with Errors

---
+++ src/01_the_basics/06_emitting_with_errors/hello.ts
@@ -0,0 +1,10 @@
+// This is an industrial-grade general-purpose greeter function:
+
+// error TS7006: Parameter 'person' implicitly has an 'any' type.
+// error TS7006: Parameter 'date' implicitly has an 'any' type.
+// function greet(person, date) {
+//   console.log(`Hello ${person}, today is ${date}!`);
+// }
+
+// error TS2554: Expected 2 arguments, but got 1.
+// greet("Brendan")
