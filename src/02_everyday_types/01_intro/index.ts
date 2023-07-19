c3aa3190f7bb50653e400cfef2f5fd8b23455f31
Gabriel MP <contato.gabrielmartinspadoin@gmail.com>
2023-07-19 00:19:45 -0300

    [TSHB] everyday_types - feat: the primitives, arrays and any

---
+++ src/02_everyday_types/01_intro/index.ts
@@ -0,0 +1,12 @@
+// In this chapter, we’ll cover some of the most common types of values you’ll
+// find in JavaScript code, and explain the corresponding ways to describe those
+// types in TypeScript. This isn’t an exhaustive list, and future chapters will
+// describe more ways to name and use other types.
+
+// Types can also appear in many more places than just type annotations. As we
+// learn about the types themselves, we’ll also learn about the places where we
+// can refer to these types to form new constructs.
+
+// We’ll start by reviewing the most basic and common types you might encounter 
+// when writing JavaScript or TypeScript code. These will later form the core 
+// building blocks of more complex types.
